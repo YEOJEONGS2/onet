99abdc68610954cd527b4f8ca9a2c03c87b3343c
yoog1219 <yoog1219@gmail.com>
2024-07-09 17:52:27 +0900

    컨텐츠, 메인 작성
    스와이퍼 배너 적용
    카테고리 썸네일 적용
    중간 배너 만들기

---
+++ js/main.js
@@ -0,0 +1,20 @@
+window.onload = function () {
+    var swiper = new Swiper(".slideBn", {
+        spaceBetween: 30,
+        centeredSlides: true,
+        autoplay: {
+          delay: 2500,
+          disableOnInteraction: false,
+        },
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      });
+  
+}
+ 
