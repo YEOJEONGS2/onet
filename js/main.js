853956e97790412c99a1cad5dd1bbc513671a69e
yoog1219 <yoog1219@gmail.com>
2024-07-10 18:00:14 +0900

    스와이퍼 새로 추가적용 베스트디자이너 폼 작성

--- js/main.js
+++ js/main.js
@@ -1,20 +1,46 @@
-window.onload = function () {
-    var swiper = new Swiper(".slideBn", {
-        spaceBetween: 30,
-        centeredSlides: true,
-        autoplay: {
-          delay: 2500,
-          disableOnInteraction: false,
-        },
-        pagination: {
-          el: ".swiper-pagination",
-          clickable: true,
-        },
-        navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-        },
-      });
+// window.onload = function () {
+//     var swiper = new Swiper(".slideBn", {
+//         spaceBetween: 30,
+//         centeredSlides: true,
+//         autoplay: {
+//           delay: 2500,
+//           disableOnInteraction: false,
+//         },
+//         pagination: {
+//           el: ".swiper-pagination",
+//           clickable: true,
+//         },
+//         navigation: {
+//           nextEl: ".swiper-button-next",
+//           prevEl: ".swiper-button-prev",
+//         },
+//       });
   
+// }
+window.onload = function () {
+  var swiper = new Swiper(".slideBn", {
+    slidesPerView: 3,
+    observer: true,
+    observeParents: true,
+    spaceBetween: 24,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    // pagination: {
+    //   el: ".swiper-pagination",
+    //   type: "progressbar",
+    // },
+    pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    },
+
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+      },
+    });
+
 }
- 
